fb2d071f21d69c8084e83b5ba28bf118a91e5b50
upperbyte <mx.makarenko@gmail.com>
2025-08-28 15:31:41 +0200

    lus

--- LUS/main.cpp
+++ LUS/main.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main()
 {
-    long n;
+    long n, cyfra, result = 0;
     cin >> n;
-    
-    
+
+    while (n > 0){
+        cyfra = n % 10;
+        n /= 10;
+        result = result * 10 + cyfra;
+
+    }
+    cout << result << endl;
 }
 
